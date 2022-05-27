77b4b6250feb50bd57826230b404367f66aec10a
yourgotocoder <yourgotocoder@gmail.com>
2022-05-27 16:17:18 +0530

    Optimizing the code to reduce the number of loops

--- backend/index.js
+++ backend/index.js
@@ -79,42 +79,41 @@ app.get("/get-excel-data-4th-sem", async (req, res) => {
     const client = await MongoClient.connect(process.env.DB_URL);
     const collection = client.db("feedback").collection("feedback-data");
     const data = await collection.find().toArray();
-    const justData = data.map((el) => el.data);
+    const justData = data.map((el) => el.data[0]);
     const reducedData = justData.reduce((previousValues, currentValue) => {
-        for (let element of currentValue) {
-            const indexOfSubjectSheet = previousValues.findIndex(
-                (el) => el.sheet === element.subject
-            );
-            if (indexOfSubjectSheet === -1) {
-                const sheetToBeInserted = {
-                    sheet: element.subject,
-                    columns: [
-                        { label: "CO1", value: "CO1" },
-                        { label: "CO2", value: "CO2" },
-                        { label: "CO3", value: "CO3" },
-                        { label: "CO4", value: "CO4" },
-                        { label: "CO5", value: "CO5" },
-                    ],
-                    content: [
-                        {
-                            CO1: element.CO1,
-                            CO2: element.CO2,
-                            CO3: element.CO3,
-                            CO4: element.CO4,
-                            CO5: element.CO5,
-                        },
-                    ],
-                };
-                previousValues.push(sheetToBeInserted);
-            } else if (indexOfSubjectSheet !== -1) {
-                previousValues[indexOfSubjectSheet].content.push({
-                    CO1: element.CO1,
-                    CO2: element.CO2,
-                    CO3: element.CO3,
-                    CO4: element.CO4,
-                    CO5: element.CO5,
-                });
-            }
+        const indexOfSubjectSheet = previousValues.find(
+            (el) => el.sheet === currentValue.subject
+        );
+
+        if (indexOfSubjectSheet === -1) {
+            const sheetToBeInserted = {
+                sheet: element.subject,
+                columns: [
+                    { label: "CO1", value: "CO1" },
+                    { label: "CO2", value: "CO2" },
+                    { label: "CO3", value: "CO3" },
+                    { label: "CO4", value: "CO4" },
+                    { label: "CO5", value: "CO5" },
+                ],
+                content: [
+                    {
+                        CO1: element.CO1,
+                        CO2: element.CO2,
+                        CO3: element.CO3,
+                        CO4: element.CO4,
+                        CO5: element.CO5,
+                    },
+                ],
+            };
+            previousValues.push(sheetToBeInserted);
+        } else if (indexOfSubjectSheet !== -1) {
+            previousValues[indexOfSubjectSheet].content.push({
+                CO1: element.CO1,
+                CO2: element.CO2,
+                CO3: element.CO3,
+                CO4: element.CO4,
+                CO5: element.CO5,
+            });
         }
         return previousValues;
     }, []);
