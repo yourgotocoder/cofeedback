cf9fec2c68f4fd5874e6efd640b027a4c9496288
yourgotocoder <yourgotocoder@gmail.com>
2022-05-27 11:12:57 +0530

    Done with semester pre-selection

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
-import MainPage from "./components/SixthSem/MainPage";
+import MainPage6thSem from "./components/SixthSem/MainPage";
+import MainPage4thSem from "./components/FourthSem/MainPage";
+import SelectSemester from "./components/SelectSemester";
 
 function App() {
-    return <MainPage />;
+    const [semester, setSemester] = useState<number | null>(null);
+
+    const handleSemesterSelected = (semester: number) => {
+        setSemester(semester);
+    };
+
+    return (
+        <>
+            {semester === null && (
+                <SelectSemester
+                    handleSemesterSelected={handleSemesterSelected}
+                />
+            )}
+            {semester === 4 ? <MainPage4thSem /> : <MainPage6thSem />}
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/SelectSemester.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import CardActions from "@mui/material/CardActions";
+import Button from "@mui/material/Button";
+
+type Props = {
+    handleSemesterSelected: (selectedSemester: number) => void;
+};
+
+const SelectSemester = (props: Props) => {
+    const semesters: number[] = [4, 6];
+
+    return (
+        <Box
+            sx={{
+                width: "100vw",
+                height: "100vh",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+            <Card
+                sx={{
+                    minWidth: "50vw",
+                    height: "50vh",
+                    textAlign: "center",
+                    padding: "2rem",
+                }}
+            >
+                <CardContent>
+                    <Typography sx={{ fontSize: 20 }}>
+                        Please Select Your Semester
+                    </Typography>
+                </CardContent>
+                <CardActions sx={{ display: "flex", flexDirection: "column" }}>
+                    {semesters.map((sem) => (
+                        <div style={{ marginTop: "1.2rem" }}>
+                            <Button
+                                variant="contained"
+                                onClick={() =>
+                                    props.handleSemesterSelected(sem)
+                                }
+                            >
+                                {sem === 4 ? "Fourth Sem" : "Sixth Sem"}
+                            </Button>
+                        </div>
+                    ))}
+                </CardActions>
+            </Card>
+        </Box>
+    );
+};
+
+export default SelectSemester;
