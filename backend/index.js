fb685c3c3b73a0256e6a896b3ce06f890387f38c
yourgotocoder <yourgotocoder@gmail.com>
2022-06-09 15:10:39 +0530

    API endpoint to retrieve 6th sem data added

--- backend/index.js
+++ backend/index.js
@@ -132,6 +132,65 @@ app.get("/get-excel-data-4th-sem", async (req, res) => {
     res.json({ data: reducedData });
 });
 
+app.get("/get-excel-data-6th-sem", async (req, res) => {
+    const client = await MongoClient.connect(process.env.DB_URL);
+    const collection = client
+        .db("feedback")
+        .collection("feedback-data-6th-sem");
+    const data = await collection.find().toArray();
+    const justData = data.reduce((previousValues, currentValue) => {
+        const array = currentValue.data;
+        for (let element of array) {
+            previousValues.push(element);
+        }
+        return previousValues;
+    }, []);
+    const reducedData = justData.reduce(
+        (previousValues, currentValue, currentIndex) => {
+            currentValue.subject = currentValue.subject.substring(0, 30);
+            const indexOfSubjectSheet = previousValues.findIndex(
+                (el) => el.sheet === currentValue.subject
+            );
+            if (indexOfSubjectSheet === -1) {
+                const sheetToBeInserted = {
+                    sheet: currentValue.subject,
+                    columns: [
+                        { label: "Sno.", value: "SNo" },
+                        { label: "CO1", value: "CO1" },
+                        { label: "CO2", value: "CO2" },
+                        { label: "CO3", value: "CO3" },
+                        { label: "CO4", value: "CO4" },
+                        { label: "CO5", value: "CO5" },
+                    ],
+                    content: [
+                        {
+                            SNo: 1,
+                            CO1: currentValue.CO1,
+                            CO2: currentValue.CO2,
+                            CO3: currentValue.CO3,
+                            CO4: currentValue.CO4,
+                            CO5: currentValue.CO5,
+                        },
+                    ],
+                };
+                previousValues.push(sheetToBeInserted);
+            } else if (indexOfSubjectSheet !== -1) {
+                previousValues[indexOfSubjectSheet].content.push({
+                    SNo: previousValues[indexOfSubjectSheet].content.length + 1,
+                    CO1: currentValue.CO1,
+                    CO2: currentValue.CO2,
+                    CO3: currentValue.CO3,
+                    CO4: currentValue.CO4,
+                    CO5: currentValue.CO5,
+                });
+            }
+            return previousValues;
+        },
+        []
+    );
+    res.json({ data: reducedData });
+});
+
 app.get("", (req, res) => {
     res.json({ message: "Hello from backend" });
 });
