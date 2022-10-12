6ee225140e23ef5b75117598fb2d3911db769293
yourgotocoder <yourgotocoder@gmail.com>
2022-10-12 12:56:05 +0530

    Sevent sem frontend work done, have to implement a success submission case and fix all the sem realated data

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import "./App.css";
-// import MainPage6thSem from "./components/SixthSem/MainPage";
+import MainPage3rdSem from "./components/3rdSem/MainPage";
 // import MainPage4thSem from "./components/FourthSem/MainPage";
 import MainPage5thSem from "./components/5thSem/MainPage";
-import MainPage3rdSem from "./components/3rdSem/MainPage";
+// import MainPage6thSem from "./components/SixthSem/MainPage";
+import MainPage7thSem from "./components/7thSem/MainPage"
 import SelectSemester from "./components/SelectSemester";
 // import GetExcel from "./components/GetExcel";
 
@@ -25,7 +26,7 @@ function App() {
             {/* {semester === 4 && <MainPage4thSem />}{" "} */}
             {semester === 5 && <MainPage5thSem/>}
             {/* {semester === 6 && <MainPage6thSem />} */}
-            {/* {semester === 7 && <MainPage7thSem/>} */}
+            {semester === 7 && <MainPage7thSem/>}
             {/* <GetExcel /> */}
         </>
     );
