e07c5d03aef55538eb2b1058f180fcc0ecfe5e5e
Sudarshan Rai <sudarshan@yourgotocoder.com>
2023-12-05 15:01:01 +0530

    Failed

--- src/App.tsx
+++ src/App.tsx
@@ -4,32 +4,30 @@ import MainPage3rdSem from "./components/3rdSem/MainPage";
 // import MainPage4thSem from "./components/FourthSem/MainPage";
 import MainPage5thSem from "./components/5thSem/MainPage";
 // import MainPage6thSem from "./components/SixthSem/MainPage";
-import MainPage7thSem from "./components/7thSem/MainPage"
+import MainPage7thSem from "./components/7thSem/MainPage";
 import SelectSemester from "./components/SelectSemester";
-// import GetExcel from "./components/GetExcel";
+import GetExcel from "./components/helpers/GetExcel";
 
 function App() {
-    const [semester, setSemester] = useState<number | null>(null);
+  const [semester, setSemester] = useState<number | null>(null);
 
-    const handleSemesterSelected = (semester: number) => {
-        setSemester(semester);
-    };
+  const handleSemesterSelected = (semester: number) => {
+    setSemester(semester);
+  };
 
-    return (
-        <>
-            {semester === null && (
-                <SelectSemester
-                    handleSemesterSelected={handleSemesterSelected}
-                />
-            )}
-            {semester === 3 && <MainPage3rdSem/>}
-            {/* {semester === 4 && <MainPage4thSem />}{" "} */}
-            {semester === 5 && <MainPage5thSem/>}
-            {/* {semester === 6 && <MainPage6thSem />} */}
-            {semester === 7 && <MainPage7thSem/>}
-            {/* <GetExcel /> */}
-        </>
-    );
+  return (
+    <>
+      {/* {semester === null && ( */}
+      {/*   <SelectSemester handleSemesterSelected={handleSemesterSelected} /> */}
+      {/* )} */}
+      {/* {semester === 3 && <MainPage3rdSem />} */}
+      {/* {semester === 4 && <MainPage4thSem />}{" "} */}
+      {/* {semester === 5 && <MainPage5thSem />} */}
+      {/* {semester === 6 && <MainPage6thSem />} */}
+      {/* {semester === 7 && <MainPage7thSem />} */}
+      <GetExcel />
+    </>
+  );
 }
 
 export default App;
--- src/components/helpers/GetExcel.tsx
+++ src/components/helpers/GetExcel.tsx
@@ -5,103 +5,103 @@ import Button from "@mui/material/Button";
 type Props = {};
 
 const transformData = (data: any) => {
-    const arrayWithTotals = data.data.map((element: any) => {
-        let objToBeInserted = {
-            SNo: "Avg",
-            CO1: 0,
-            CO2: 0,
-            CO3: 0,
-            CO4: 0,
-            CO5: 0,
-        };
-        for (let content of element.content) {
-            objToBeInserted.CO1 += content.CO1;
-            objToBeInserted.CO2 += content.CO2;
-            objToBeInserted.CO3 += content.CO3;
-            objToBeInserted.CO4 += content.CO4;
-            objToBeInserted.CO5 += content.CO5;
-        }
-        objToBeInserted.CO1 = +(
-            objToBeInserted.CO1 / element.content.length
-        ).toFixed(3);
-        objToBeInserted.CO2 = +(
-            objToBeInserted.CO2 / element.content.length
-        ).toFixed(3);
-        objToBeInserted.CO3 = +(
-            objToBeInserted.CO3 / element.content.length
-        ).toFixed(3);
-        objToBeInserted.CO4 = +(
-            objToBeInserted.CO4 / element.content.length
-        ).toFixed(3);
-        objToBeInserted.CO5 = +(
-            objToBeInserted.CO5 / element.content.length
-        ).toFixed(3);
+  const arrayWithTotals = data.data.map((element: any) => {
+    let objToBeInserted = {
+      SNo: "Avg",
+      CO1: 0,
+      CO2: 0,
+      CO3: 0,
+      CO4: 0,
+      CO5: 0,
+    };
+    for (let content of element.content) {
+      objToBeInserted.CO1 += content.CO1;
+      objToBeInserted.CO2 += content.CO2;
+      objToBeInserted.CO3 += content.CO3;
+      objToBeInserted.CO4 += content.CO4;
+      objToBeInserted.CO5 += content.CO5;
+    }
+    objToBeInserted.CO1 = +(
+      objToBeInserted.CO1 / element.content.length
+    ).toFixed(3);
+    objToBeInserted.CO2 = +(
+      objToBeInserted.CO2 / element.content.length
+    ).toFixed(3);
+    objToBeInserted.CO3 = +(
+      objToBeInserted.CO3 / element.content.length
+    ).toFixed(3);
+    objToBeInserted.CO4 = +(
+      objToBeInserted.CO4 / element.content.length
+    ).toFixed(3);
+    objToBeInserted.CO5 = +(
+      objToBeInserted.CO5 / element.content.length
+    ).toFixed(3);
 
-        element.content.push(objToBeInserted);
-        return element;
-    });
-    return arrayWithTotals;
+    element.content.push(objToBeInserted);
+    return element;
+  });
+  return arrayWithTotals;
 };
 
 const GetExcel = (props: Props) => {
-    const [_4thSemData, set_4thSemData] = useState();
-    const [_6thSemData, set_6thSemData] = useState();
+  const [_4thSemData, set_4thSemData] = useState();
+  const [_6thSemData, set_6thSemData] = useState();
 
-    useEffect(() => {
-        fetch("http://localhost:3011/get-excel-data-4th-sem")
-            .then((response) => response.json())
-            .then((data) => {
-                const arrayWithTotals = transformData(data);
-                set_4thSemData(arrayWithTotals);
-                console.log(arrayWithTotals);
-            });
-        fetch("http://localhost:3011/get-excel-data-6th-sem")
-            .then((response) => response.json())
-            .then((data) => {
-                const arrayWithTotals = transformData(data);
-                set_6thSemData(arrayWithTotals);
-                console.log(arrayWithTotals);
-            });
-    }, []);
+  useEffect(() => {
+    fetch("http://localhost:3011/get-excel-data-4th-sem")
+      .then((response) => response.json())
+      .then((data) => {
+        const arrayWithTotals = transformData(data);
+        set_4thSemData(arrayWithTotals);
+        console.log(arrayWithTotals);
+      });
+    fetch("http://localhost:3011/get-excel-data-6th-sem")
+      .then((response) => response.json())
+      .then((data) => {
+        const arrayWithTotals = transformData(data);
+        set_6thSemData(arrayWithTotals);
+        console.log(arrayWithTotals);
+      });
+  }, []);
 
-    const handleDownload = () => {
-        const setting = {
-            fileName: "4thSemFeedbackData",
-            extraLength: 3,
-        };
-        if (_4thSemData) {
-            xlsx(_4thSemData, setting);
-        }
+  const handleDownload = () => {
+    const setting = {
+      fileName: "4thSemFeedbackData",
+      extraLength: 3,
     };
+    if (_4thSemData) {
+      xlsx(_4thSemData, setting);
+    }
+  };
 
-    const handle6thDownload = () => {
-        const setting = {
-            fileName: "6thSemFeedbackData",
-            extraLength: 3,
-        };
-        if (_6thSemData) {
-            xlsx(_6thSemData, setting);
-        }
+  const handle6thDownload = () => {
+    const setting = {
+      fileName: "6thSemFeedbackData",
+      extraLength: 3,
     };
+    if (_6thSemData) {
+      xlsx(_6thSemData, setting);
+    }
+  };
 
-    return (
-        <>
-            <Button
-                variant="contained"
-                onClick={handleDownload}
-                disabled={_4thSemData === undefined}
-            >
-                GetExcel4th
-            </Button>
-            <Button
-                variant="contained"
-                onClick={handle6thDownload}
-                disabled={_6thSemData === undefined}
-            >
-                GetExcel6th
-            </Button>
-        </>
-    );
+  return (
+    <>
+      <Button
+        variant="contained"
+        onClick={handleDownload}
+        disabled={_4thSemData === undefined}
+      >
+        GetExcel4th
+      </Button>
+      <Button
+        variant="contained"
+        onClick={handle6thDownload}
+        disabled={_6thSemData === undefined}
+      >
+        GetExcel6th
+      </Button>
+    </>
+  );
 };
 
 export default GetExcel;
